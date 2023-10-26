3721d3c37fe69709366d22985cc3ea318f73962d
Longcup <1966875241@qq.com>
2023-10-27 00:02:48 +0800

    v1.0  基本功能完成，许多功能有待完善

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CQU_Wifi-Login-Script
+请在连接着校园网的情况下运行一次程序并不要删除request_info.txt文件  
+之后运行程序即可连接校园网
--- test.py
+++ test.py
@@ -7,40 +7,69 @@ import time
 import socket
 import json
 
-
 data_s = {
-        "c": "Portal",
-        "a": "login",
-        "callback": "dr1004",
-        "login_method": "1",
-        "user_account": "20212801",
-        "user_password": "021208",
-        "wlan_user_ip": "172.22.131.149",
-        "wlan_user_ipv6": "",
-        "wlan_user_mac":"c03c59454e4e",
-        "wlan_ac_ip": "",
-        "wlan_ac_name":"",
-        "jsVersion": "4.2.2",
-        "terminal_type": "1",
-        "lang": "zh_cn",
-        "v": "4167"
-        #"lang": "zh"
+    "c": "Portal",
+    "a": "login",
+    "callback": "dr1004",
+    "login_method": "1",
+    "user_account": "",
+    "user_password": "",
+    "wlan_user_ip": "",
+    "wlan_user_ipv6": "",
+    "wlan_user_mac": "",
+    "wlan_ac_ip": "",
+    "wlan_ac_name": "",
+    "jsVersion": "4.2.2",
+    "terminal_type": "1",
+    "lang": "zh_cn",
+    "v": "4167"
+    # "lang": "zh"
 }
-if __name__ == "__main__":
-    data_r = {}
-    data_r = {'result': 1, 'msg': '获取用户在线信息成功！', 'list': [{'online_session': 43360, 'online_time': '2023-10-26 19:07:47', 'online_ip': '172.22.131.149', 'online_ipv6': '', 'online_mac': 'c03c59454e4e', 'time_long': '524', 'uplink_bytes': '0', 'downlink_bytes': '799', 'dhcp_host': '', 'device_alias': '', 'nas_ip': '67570186', 'user_account': '20212801', 'exit_id': '0', 'is_radius_auth': '0', 'is_perceive': '0', 'phone_flag': '0', 'is_bind_mac': '0', 'is_owner_ip': '1'}], 'total': 1}
+data_r = {}
 
-    # session = requests.Session()
-    # response = requests.get("http://login.cqu.edu.cn:801/eportal/portal/?c=Portal&a=login&callback=dr1004&login_method=1&user_account=%2C0%2C20212801&user_password=021208&wlan_user_ip=172.24.10.8&wlan_user_ipv6=&wlan_user_mac=38f3ab903304&wlan_ac_ip=&wlan_ac_name=&jsVersion=4.2.2&terminal_type=1&lang=zh-cn&v=4643&lang=zh")
-    response = requests.get("http://login.cqu.edu.cn:801/eportal/portal/", params=data_s)
-    print(response.text)
 
+def get_inf() :
+    # 连上校园网后运行
     response = requests.get(
         'http://login.cqu.edu.cn:801/eportal/portal/online_list')
     print(json.loads(response.text[12:-2]))
+    with open('request_info.txt', 'w') as f:
+        f.write(response.text[12:-2])
 
-    # url = response.url
-    #
-    # with open('request_host_url.txt', 'w') as f:
-    #     f.write(url)
+
+
+def login():
+    with open("request_info.txt", "r") as f:
+        data_r = json.loads(f.read())
+    print(data_r)
+
+    data_s["wlan_user_ip"] = data_r["list"][0]["online_ip"]
+    data_s["wlan_user_mac"] = data_r["list"][0]["online_mac"]
+
+    data_s["user_account"] = input("输入账号:")
+    data_s["user_password"] = input("输入密码:")
+    response = requests.get("http://login.cqu.edu.cn:801/eportal/portal/", params=data_s)
+    print(response.text)
+
+
+def is_net_ok() -> bool:
+    try:
+        status = requests.get("https://www.baidu.com").status_code
+        if status == 200:
+            print("对对对")
+            return True
+        else:
+            return False
+    except Exception:
+        return False
+
+
+if __name__ == "__main__":
+    if is_net_ok():
+        data_r = get_inf()
+    else:
+        login()
+
+    # session = requests.Session()
+    # response = requests.get("http://login.cqu.edu.cn:801/eportal/portal/?c=Portal&a=login&callback=dr1004&login_method=1&user_account=%2C0%2C20212801&user_password=021208&wlan_user_ip=172.24.10.8&wlan_user_ipv6=&wlan_user_mac=38f3ab903304&wlan_ac_ip=&wlan_ac_name=&jsVersion=4.2.2&terminal_type=1&lang=zh-cn&v=4643&lang=zh")
 
